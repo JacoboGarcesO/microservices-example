c4e1f03d55e9c807a96ae90973ffba97582f0b42
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2025-08-05 17:42:34 -0500

    .

--- api-gateway/src/main/java/org/example/apigateway/config/RouteConfig.java
+++ api-gateway/src/main/java/org/example/apigateway/config/RouteConfig.java
@@ -1,5 +1,6 @@
 package org.example.apigateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -7,11 +8,14 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RouteConfig {
+  @Value("${products-service.url}") private String productsServiceUrl;
+  @Value("${products-service.id}") private String productsServiceId;
+  @Value("${products-service.path}") private String productsServicePath;
 
   @Bean
   public RouteLocator createRouteLocator(RouteLocatorBuilder builder) {
     return builder.routes()
-      .route("", route -> route.path("/api/products/**").uri("http://localhost:8081"))
+      .route(productsServiceId, route -> route.path(productsServicePath).uri(productsServiceUrl))
       .build();
   }
 }
